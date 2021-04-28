6089a22c48c5868a7e6f49fccf7e497e5cb57b38
Burak Mike <burakmahyk@gmail.com>
2021-04-28 21:40:57 +0300

    added gsap animation

--- components/Technologies.vue
+++ components/Technologies.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
-    <h1 class="font-bold text-xl text-center mb-4">
+    <h1 class="font-bold text-xl text-center mb-4 technologies-box">
       İlgilendiğim Teknolojiler
     </h1>
     <div class="flex flex-wrap items-center md:justify-evenly justify-center">
-      <technologies-container :href="JavaScriptLink" :name="'JavaScript'">
+      <technologies-container :href="JavaScriptLink"  class="technologies-box" :name="'JavaScript'">
         <IconJs fill="white" />
       </technologies-container>
-      <technologies-container :href="nodeLink" :name="'Node Js'">
+      <technologies-container :href="nodeLink" class="technologies-box" :name="'Node Js'">
         <IconNode fill="#32B67A" />
       </technologies-container>
-      <technologies-container :href="vueLink" :name="'Vue Js'">
+      <technologies-container :href="vueLink" class="technologies-box" :name="'Vue Js'">
         <IconVue fill="#17C37B" />
       </technologies-container>
-      <technologies-container :href="tailwindLink" :name="'Tailwind'">
+      <technologies-container :href="tailwindLink" class="technologies-box" :name="'Tailwind'">
         <IconTailwind fill="#0033FF" />
       </technologies-container>
-      <technologies-container :href="figmaLink" :name="'Figma'">
+      <technologies-container :href="figmaLink" class="technologies-box" :name="'Figma'">
         <IconFigma fill="red" />
       </technologies-container>
     </div>
@@ -30,6 +30,8 @@ import IconVue from '../assets/icons/vue-dot-js.svg'
 import IconFigma from '../assets/icons/figma.svg'
 import IconTailwind from '../assets/icons/tailwindcss.svg'
 import TechnologiesContainer from './technologies-container'
+import {gsap} from 'gsap'
+import {ScrollTrigger} from 'gsap/dist/ScrollTrigger'
 export default {
   name: 'Technologies',
   components: {
@@ -49,6 +51,61 @@ export default {
       JavaScriptLink: 'https://www.javascript.com/',
     }
   },
+  mounted() {
+    this.ScrollAnimation()
+  },
+  methods:{
+    ScrollAnimation(){
+      gsap.registerPlugin(ScrollTrigger)
+      gsap.from('.technologies-box',{
+        scrollTrigger:{
+          trigger: '.technologies',
+          start: "top center",
+          end: "60% center",
+          scrub: true,
+          markers: true
+        },
+        opacity:0,
+        delay: 1,
+        stagger: 0.3,
+        ease: 'elastic',
+        scale:1,
+        duration: 5
+
+      })
+      gsap.to('.technologies-box',{
+        scrollTrigger:{
+          trigger: '.technologies',
+          start:"10px 20%",
+          end: "65% 200px",
+          scrub: true,
+
+        },
+        opacity:0,
+        y: -100,
+        stagger: 0.1,
+        ease: 'back.out'
+      })
+
+      //tec animation
+      gsap.to('.technologies',{
+        scrollTrigger:{
+          trigger: '.technologies',
+          start: "top center",
+          end: "center center",
+          scrub: true,
+
+        },
+        opacity:1,
+        y:-300,
+        scale:1,
+        duration: 5
+
+      })
+    }
+  }
+
+
 }
 </script>
 
