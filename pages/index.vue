937084b834569a83ad45d8048b68e22f1794a44e
Burak Mike <burakmahyk@gmail.com>
2021-04-28 21:41:04 +0300

    major design changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="space-y-20">
-    <div class="sc space-y-20">
-      <div class="space-y-8">
-        <h1 class="text-2xl font-bold text-highlight">
-          Ben Burak Mike.<br />
-          Kendini Front-end teknolojileri alanında geliştiren öğrenci ve
-          developerım.
-        </h1>
-        <p class="text-xl">
-          Front-end teknolojileri, tasarım araçları gibi konulara ilgiliyim.
+    <div class="sc h-screen flex items-center justify-center pb-24">
+      <div class="space-y-12">
+        <nuxt-link to="/notes starter">
+          <typical
+            class="text-6xl font-bold text-highlight text-center hover:text-green-300 "
+            :steps="['{Merhaba',800 , '{Merhaba Dünya!}', 500, '{Ben Burak 👋🏽}']"
+            :wrapper="'h1'"
+          ></typical>
+        </nuxt-link>
+
+        <p class="text-2xl text-center description">
+          Front-end teknolojileri, tasarım araçları gibi konulara ilgiliyim
         </p>
       </div>
-      <div class="mt-10">
-        <Technologies />
-      </div>
     </div>
-    <div class="pt-10 pb-4">
+
+    <div class="h-screen sc pt-12 flex items-center technologies opacity-0">
+      <Technologies class="w-full"/>
+    </div>
+    <div class="pt-10 pb-4 h-screen flex items-center justify-center projects">
+      <div>
       <h1 class="text-center font-bold text-xl mb-4">Projeler</h1>
       <div class="flex flex-wrap justify-center gap-4 sc-large">
         <ProjectContainer
@@ -24,11 +29,13 @@
         />
         <ProjectContainer :name="'personal_website'" />
       </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import typical from 'vue-typical'
 import ProjectContainer from '../components/Project-container'
 import Technologies from '../components/Technologies'
 export default {
@@ -36,6 +43,7 @@ export default {
   components: {
     ProjectContainer,
     Technologies,
+    typical
   },
 }
 </script>
