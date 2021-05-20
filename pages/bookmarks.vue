ec71ea06cbd3b721d0ad58fb87d58687314e72e7
Burak Mike <burakmahyk@gmail.com>
2021-05-20 22:27:14 +0300

    added bookmark page and raindrop integration

---
+++ pages/bookmarks.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="sc">
+   <div class="space-y-4">
+     <typical
+       class="text-2xl font-medium text-highlight"
+       :steps="[
+              '{',
+              500,
+              '{ Dikkatimi Çekenler }',
+            ]"
+       :wrapper="'h1'"
+     ></typical>
+     <p>İnternetten gezinirken dikkatimi çeken yararlı olacağını düşündüğüm linkler</p>
+   </div>
+    <div class="pt-10 space-y-4">
+      <div v-for="bookmark in bookmarks" :key="bookmark._id" class="flex space-x-2 hover:shadow-xl p-4 border-2 border-gray-100 dark:border-gray-800 rounded">
+        <a :href="bookmark.link" class="flex space-x-4">
+        <span class="backdrop-grayscale">
+           <img :src="bookmark.cover" alt="bookmark._id" class="w-24">
+        </span>
+
+        <div>
+          <h1 class="text-xl text-highlight">{{bookmark.title}}</h1>
+          <p>{{bookmark.domain}}・{{formatDate(bookmark.created)}}</p>
+        </div>
+        </a>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {DateTime} from 'luxon'
+import {getBookmarks} from '../lib/raindrop'
+import typical from 'vue-typical'
+export default {
+  name: 'Bookmarks',
+  data(){
+    return{
+      bookmarks:null
+    }
+  },
+
+  components:{
+    typical,
+  },
+  created() {
+      this.getBookmarks()
+  },
+  methods:{
+    async getBookmarks(){
+      this.bookmarks = await getBookmarks()
+    },
+    formatDate(date){
+      return DateTime.fromISO(date).toRelative()
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
