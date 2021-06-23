c667f0783724c7117aad526ea6262c0ea39161ac
Burak Mike <burakmahyk@gmail.com>
2021-06-23 12:26:07 +0300

    Create Loading

---
+++ components/Loading.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-if="isLoading" class="flex justify-center items-center">
+      <span class="text-highlight text-2xl animate-pulse">Yükleniyor</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+  props: {
+    isLoading: {
+      type: Boolean,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped></style>
