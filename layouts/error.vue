4ad83e381791b4ec7324d2188cadb38d73a6fedf
Burak Mike <64980398+MahykBurak@users.noreply.github.com>
2021-11-28 17:16:39 +0300

    added 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class='sc h-screen flex items-center justify-center'>
+   <h1 class='text-8xl font-bold animate-pulse'>404</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'error' // you can set a custom layout for the error page
+}
+</script>
