eef0c0bcd94151d776b67a11502f23b734da99f0
Burak Mike <64980398+MahykBurak@users.noreply.github.com>
2021-11-28 16:08:06 +0300

    added photos

--- pages/photos.vue
+++ pages/photos.vue
@@ -1,19 +1,35 @@
 <template>
-  <div>
-
+  <div class='sc-large'>
+  <Loading :is-loading='isLoading'/>
+    <div v-if='!  isLoading' class='grid lg:grid-cols-3 sm:grid-cols-2'>
+      <div v-for='photo in data'>
+        <img :src='photo.urls.regular' alt='photo.alt-description'>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Unsplash from '../lib/unsplash.js'
+import Loading from '../components/Loading'
 export default {
   name: 'Photos',
+  data(){
+    return{
+      data:[],
+      isLoading: true
+    }
+  },
+  components:{
+    Loading,
+  },
   created() {
     this.getPhotos()
   },
   methods:{
     async getPhotos(){
-      console.log( await Unsplash.getPhotos());
+      this.data = await Unsplash.getPhotos()
+      this.isLoading = false;
     }
   }
 }
