ed0e6369b5873455a60fa6c694d2a334fb41b315
Burak Mike <64980398+MahykBurak@users.noreply.github.com>
2021-11-29 14:17:25 +0300

    added gif

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,5 +1,6 @@
 <template>
   <div class='sc h-screen flex items-center justify-center flex-col'>
+    <img src="https://media2.giphy.com/media/9J7tdYltWyXIY/giphy.gif?cid=ecf05e47jxzgrm9peko88ql9uv06c1sktoe6v3gcxeii6u94&amp;rid=giphy.gif&amp;ct=g" alt="sad vincent vega GIF" style="width: 500px; height: 421.1111111111111px; opacity: 1; left: 0px; top: 0px;">
    <h1 class='text-8xl font-bold animate-pulse'>404</h1>
     <span class='text-3xl font-bold'>{{error.message}}</span>
   </div>
