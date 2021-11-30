881d53cfff167cdbe82baf5f7dc85231e70b6f2b
Burak Mike <64980398+MahykBurak@users.noreply.github.com>
2021-11-30 13:41:37 +0300

    Update photos.vue

--- pages/photos.vue
+++ pages/photos.vue
@@ -1,9 +1,12 @@
 <template>
   <div class='sc-large'>
-  <Loading :is-loading='isLoading'/>
+    <Loading :is-loading='isLoading'/>
+
     <div v-if='!  isLoading' class='grid lg:grid-cols-3 sm:grid-cols-2'>
       <div v-for='photo in data'>
-        <img :src='photo.urls.regular' alt='photo.alt-description'>
+        <a :href='photo.links.html'>
+        <img :src='photo.urls.regular' :alt='photo.alt-description' >
+        </a>
       </div>
     </div>
   </div>
@@ -26,6 +29,7 @@ export default {
   created() {
     this.getPhotos()
   },
+
   methods:{
     async getPhotos(){
       this.data = await Unsplash.getPhotos()
