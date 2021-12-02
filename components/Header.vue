dcf6e16410ebf90c535a6e25b9f0a167458764ce
Burak Mike <64980398+MahykBurak@users.noreply.github.com>
2021-12-02 20:42:16 +0300

    nav items added to an array

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,9 @@
 <template>
   <header class="sc flex justify-center md:block py-6 z-50">
     <nav class="space-x-4 flex">
-      <NavItem :path="'/'" :name="'.giriş()'" />
-      <NavItem :path="'/notes'" :name="'.notlar()'" />
-      <NavItem :name="'.galeri()'" :path="'/photos'"/>
-      <NavItem :path="'/bookmarks'" :name="'.yerİmleri()'" /> 
+      <template v-for='navItem in navItems'>
+        <NavItem :name='navItem.name' :path='navItem.path'/>
+      </template>
     </nav>
   </header>
 </template>
@@ -14,6 +13,17 @@ import NavItem from './Nav-item'
 export default {
   name: 'Header',
   components: { NavItem },
+  data(){
+    return{
+      navItems: [
+        {path:'/',name:'.giriş()'},
+        {path:'/notes',name:'.notlar()'},
+        {path:'/projects',name:'.projeler()'},
+        {path:'/photos',name:'.galeri()'},
+        {path:'/bookmarks',name:'.yerİmleri()'}
+      ]
+    }
+  }
 }
 </script>
 
