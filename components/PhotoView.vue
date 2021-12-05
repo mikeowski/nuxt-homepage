4dfa14a917c378827753650536638082d4acf051
Burak Mike <64980398+MahykBurak@users.noreply.github.com>
2021-12-05 19:01:09 +0300

    create unsplash photo layout

---
+++ components/PhotoView.vue
@@ -0,0 +1,36 @@
+<template>
+    <div v-if="!isLoading" class="grid lg:grid-cols-3 sm:grid-cols-2 sc-large">
+      <div v-for="photo in photoData" :key="photo.id">
+        <a :href="photo.links.html">
+          <img :src="photo.urls.regular" :alt="photo.description" />
+        </a>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name:"PhotoView",
+  props:{
+    photoData:{
+      type:Array,
+      required:true,
+    }
+  },
+  data(){
+    return{
+      photoData:[],
+    }
+  },
+  created() {
+
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
