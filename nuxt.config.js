d9a50a473bea5186325c5f8202f05e5578d779c4
Burak Mike <64980398+MahykBurak@users.noreply.github.com>
2022-01-06 14:38:24 +0300

    removed notes page

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/auth.js'],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -31,14 +31,14 @@ export default {
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
-  serverMiddleware: [{ path: '/api/comment', handler: '~/api/comment.js' }],
+  serverMiddleware: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
-    '@nuxt/content',
+
     '@nuxtjs/dotenv',
     'nuxt-svg-loader',
   ],
